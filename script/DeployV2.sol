04fef74cc396648aa82166154a790b620a4e8774
adijdhv <aadeshjadhav14@gmail.com>
2025-05-20 16:21:56 +0530

    updated v2 deploy script

---
+++ script/DeployV2.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/VirtualsSniper.sol";
+
+contract DeployVirtualsSniper is Script {
+    function run() external {
+        // Load environment variables
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address routerAddress = vm.envAddress("UNISWAP_ROUTER");
+        address virtualTokenAddress = vm.envAddress("VIRTUAL_TOKEN_ADDRESS");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the contract
+        VirtualsSniper sniper = new VirtualsSniper(
+            routerAddress,
+            virtualTokenAddress
+        );
+
+        vm.stopBroadcast();
+
+        // Log important information
+        console.log("VirtualsSniper deployed at:", address(sniper));
+        console.log("Owner:", sniper.owner());
+        console.log("Router:", address(sniper.router()));
+        console.log("Virtual Token Address:", sniper.WETH());
+    }
+}
